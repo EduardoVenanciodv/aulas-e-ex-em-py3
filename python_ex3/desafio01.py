cd3a233d56a6c5639f11dc694dad499c0240d8e6
Eduardo Venancio <eduavenancio@gmail.com>
2025-10-07 15:38:27 -0300

    ex.03 Adicionado um repetidor
    Repetidor para conseguir ver mais números, escritos por extenso.

--- python_ex3/desafio01.py
+++ python_ex3/desafio01.py
@@ -5,7 +5,11 @@ n = int(input('Digite um número de 0 ao 20: '))
 while True:
     if n < 0 or n > 20:
         n = int(input('Tente novamente. Digite um número de 0 ao 20: '))
-    else:
-        break
-
-print(f'Voce digitou o número {nomenum[n]}')
+    elif n >= 0 or n <= 20:
+        print(f'Voce digitou o número \033[34m{nomenum[n]}\033[m.')
+        r = str(input('Você quer ver mais números (\033[32mSim\033[m/\033[31mNão\033[m)?')).upper().strip()[0]
+        if r == 'S':
+            n = int(input('Digite um novo número de 0 ao 20: '))
+        elif r == 'N':
+            break
+print('Fim')
