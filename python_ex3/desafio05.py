12f4f91c2c0ad86eba7275cf23613d3dfd85d04b
Eduardo Venancio <eduavenancio@gmail.com>
2025-10-08 15:31:12 -0300

    ex.05 Os valores agora são float
    Os valores agora são float, mesmo dentro da tupla sendo inteiros.

--- python_ex3/desafio05.py
+++ python_ex3/desafio05.py
@@ -1,15 +1,20 @@
-produtos = ('Tang', 1.00, 'Leite', 4.15, 'Toddy', 7.89, 'Banana', 3.56, 'Pão', 7.70, 'Tapioca', 5.70, 'Farofa', 4.99)
+produtos = ('Tang', 1,
+            'Leite', 4.15,
+            'Toddy', 7,
+            'Banana', 3.56,
+            'Pão', 7.70,
+            'Tapioca', 5.70,
+            'Farofa', 5)
 texto = 'Listagem De Preços'
 cont = 0
 print('_'*60)
 print(f'{texto:^60}')
 print('_'*60)
-
 for c in produtos:
     cont += 1
     if cont == 1:
         print(f'{c:.<50}', end='')
     elif cont == 2:
         cont = 0
-        print(f'R$ {c:>6}')
+        print(f'R$ {c:>6.2f}')
 print('_'*60)
