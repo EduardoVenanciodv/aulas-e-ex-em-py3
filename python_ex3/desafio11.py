806d277cd126744929e3e288a234fb61a9f91aec
Eduardo Venancio <eduavenancio@gmail.com>
2025-10-23 01:09:10 -0300

    ex.11 Dividindo valores em várias listas
    O usuário irar adicionar valores em uma lista, que no fim irar criar mais duas outra lista.
    No fim irar mostrar as três lista, uma completa, outra contendo apenas números pares e a outra contendo ímpares.

---
+++ python_ex3/desafio11.py
@@ -0,0 +1,14 @@
+lista = []
+saida = ''
+while saida != 'N':
+    lista.append(int(input('Digite um número: ')))
+    saida = str(input('Quer continuar (S/N)?')).upper().strip()[0]
+lista_2 = []
+lista_3 = []
+for r in lista:
+    if r % 2 == 0:
+        lista_2.append(r)
+    else:
+        lista_3.append(r)
+print(f'Primeira lista completa: {lista}\nSegunda lista só com números pares: {lista_2}'
+      f'\nTerceira lista só com ímpars: {lista_3}')
