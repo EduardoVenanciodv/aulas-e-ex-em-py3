8f56ed94af73c0f2e26826ee917d18e39aac08ce
Eduardo Venancio <eduavenancio@gmail.com>
2025-11-04 20:52:47 -0300

    ex.17 Corrigido.
    Trocado os 'for' iniciais para while true, para conseguir utilizar o 'if not in', pra não ter números repetidos dentro de cada jogo sorteado.

--- python_ex3/desafio17.py
+++ python_ex3/desafio17.py
@@ -2,19 +2,26 @@ from random import randint
 from time import sleep
 sorteio = []
 final = []
+total = 0
 print('='*40)
 print(f'{"Jogo da Mega Sena":^40}')
 print('='*40)
 x = int(input('Quantos jogos você quer que eu sorteie?'))
+while total <= x:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in sorteio:
+            sorteio.append(num)
+            cont += 1
+        if cont == 6:
+            break
+    sorteio.sort()
+    final.append(sorteio[:])
+    sorteio.clear()
+    total += 1
 print(f'{" Sorteando":*>15} {x} {"Jogos ":*<15}')
 for i in range(0, x):
-    for c in range(0, 6):
-        num = randint(0, 60)
-        sorteio.append(num)
-        if c == 5:
-            final.append(sorteio[:])
-            print(f'Jogo {i+1}: {final[i]}.')
-            sleep(1)
-            sorteio.clear()
+    print(f'Jogo {i+1}: {final[i]}.')
+    sleep(1)
 print(f'{" =":*>11} {"Boa Sorte"} {"= ":*<11}')
-print(final)
