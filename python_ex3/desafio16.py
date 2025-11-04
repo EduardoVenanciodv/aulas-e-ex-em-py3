e3607179cc060486f079ff4ccf92ebadf1727ac9
Eduardo Venancio <eduavenancio@gmail.com>
2025-11-04 02:01:26 -0300

    ex.16 Corrigido.
    Já de inicio foram adicionadas 3 lista, dentro de uma lista composta.
    Os valores pares foram somados dentro do primeiro for, junto com o maior valor da segunda coluna.
    A soma da terceira coluna foi somada diretamente do print.

--- python_ex3/desafio16.py
+++ python_ex3/desafio16.py
@@ -1,38 +1,21 @@
-matriz = []
-part = []
-part1 = []
-part2 = []
-pares = []
-cont = cont2 = maiorv = pares = 0
-for c in range(0, 9):
-    if c <= 2:
-        valor = int(input(f'Digite um numero na posição [0, {c}]'))
-        part.append(valor)
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+maiorv = pares = 0
+for l in range(0, 3):
+    for c in range(0, 3):
+        valor = int(input(f'Digite um numero na posição [{l}, {c}]:'))
+        matriz[l][c] = valor
         if valor % 2 == 0:
             pares += valor
-    elif c <= 5:
-        valor = int(input(f'Digite um numero na posição [1, {cont}]'))
-        part1.append(valor)
-        if valor % 2 == 0:
-            pares += valor
-        if c == 3:
+        if l == 1 and c == 0:
             maiorv = valor
         else:
-            if maiorv < valor:
+            if maiorv < valor and l == 1:
                 maiorv = valor
-        cont += 1
-    elif c <= 8:
-        valor = int(input(f'Digite um numero na posição [2, {cont2}]'))
-        part2.append(valor)
-        if valor % 2 == 0:
-            pares += valor
-        cont2 += 1
-    if c == 8:
-        matriz.append(part[:])
-        matriz.append(part1[:])
-        matriz.append(part2[:])
-        for m in range(0, 3):
-            print(f'{matriz[m]}')
+
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end=' ')
+    print()
 print('=_'*15)
 print(f'A soma dos pares é {pares}.')
 print(f'A soma da terceira coluna é {matriz[0][2] + matriz[1][2] + matriz[2][2]}.')
