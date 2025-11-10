58d62134ae893b90959572b447997ba1662a56f1
Eduardo Venancio <eduavenancio@gmail.com>
2025-11-10 17:29:39 -0300

    ex.19 Corrigido
    Mudança na exibição final e foi adicionado mais uma situação que é a recuperação.

--- python_ex3/desafio19.py
+++ python_ex3/desafio19.py
@@ -1,10 +1,12 @@
 aluno = dict()
 aluno['Nome'] = str(input('Nome:'))
 aluno['Média'] = float(input(f'Média de {aluno["Nome"]}:'))
-if aluno['Média'] >= 6:
+if aluno['Média'] >= 7:
     aluno['Situação'] = 'Aprovado'
-elif aluno['Média'] < 6:
+elif aluno['Média'] < 5:
     aluno['Situação'] = 'Reprovado'
+elif aluno['Média'] < 7:
+    aluno['Situação'] = 'Recuperação'
+print('=-'*25)
 for a, r in aluno.items():
-    print(f'{a} é igual a {r}.')
-print(aluno)
+    print(f'{"-":>3} {a} é igual a {r}.')
