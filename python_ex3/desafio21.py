554d18e7b30e8daaf373d6afd3b40632f906b90a
Eduardo Venancio <eduavenancio@gmail.com>
2025-11-12 20:56:08 -0300

    ex.21 Corrigido
    Mudança no ano que o usuário nasceu. Agora não é adicionado dentro do dicionário, e é utilizado para fazer o cálculo para da 'Idade' e 'Aposentadoria'.

--- python_ex3/desafio21.py
+++ python_ex3/desafio21.py
@@ -2,13 +2,13 @@ from datetime import date
 cadastro = dict()
 ano = date.today()
 cadastro['Nome'] = str(input('Digite seu nome:'))
-cadastro['Nascimento'] = int(input('Digite o seu ano de nascimento: '))
-cadastro['Idade'] = ano.year - cadastro['Nascimento']
+nascimento = int(input('Digite o seu ano de nascimento: '))
+cadastro['Idade'] = ano.year - nascimento
 cadastro['CTPS'] = int(input('Digite o número da sua carteira de trabalaho (0 se você não tiver):'))
 if cadastro['CTPS'] != 0:
     cadastro['Contrato'] = int(input('Ano de contratação:'))
     cadastro['Sálario'] = float(input('Digite seu sálario:'))
-    cadastro['Aposentadoria'] = (cadastro['Contrato'] - cadastro['Nascimento']) + 35
+    cadastro['Aposentadoria'] = (cadastro['Contrato'] - nascimento) + 35
 print('=-'*20)
 for k, v in cadastro.items():
-    print(f'{k} tem o valor {v}')
+    print(f'  - {k} tem o valor {v}')
