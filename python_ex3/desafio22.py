adbaeb31a81510358c01dc8ba38866af90ca757d
Eduardo Venancio <eduavenancio@gmail.com>
2025-11-12 21:43:10 -0300

    ex.22 Corrigido
    A lista gol já adiciona diretamente os gols com append, e o diocionário 'jogador' faz uma copia direta da lista de gol com a key 'Gols'. O diocionário 'jogador' utiliza a nova key 'Total' para somar todos os gols com 'sum(gol)'.

--- python_ex3/desafio22.py
+++ python_ex3/desafio22.py
@@ -1,14 +1,11 @@
 jogador = {}
 gol = list()
-tot = 0
 jogador['Nome'] = str(input('Digite o nome do jogador: '))
 jogos = int(input(f'Digite quantos jogos o {jogador['Nome']} jogou:'))
 for c in range(0, jogos):
-    jogogol = int(input(f'Quantos gols na partida {c}:'))
-    gol.append(jogogol)
-    jogador['Gols'] = gol
-    tot += jogogol
-    jogador['Total'] = tot
+    gol.append(int(input(f'Quantos gols na partida {c}:')))
+jogador['Gols'] = gol[:]
+jogador['Total'] = sum(gol)
 print('=-'*20)
 for k, v in jogador.items():
     print(f'O campo {k} tem o valor {v}.')
@@ -16,3 +13,4 @@ print('=-'*20)
 print(f'O jogador {jogador["Nome"]} jogou {jogos} partidas. ')
 for n, g in enumerate(jogador['Gols']):
     print(f'{"=>":>6}Na partida {n}, fez {g} gols.')
+print(f'Foi uma total de {jogador["Total"]} gols.')
