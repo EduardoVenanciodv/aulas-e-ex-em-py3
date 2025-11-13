a91ffcf08a5eb9c98158c15a755001073c353e9d
Eduardo Venancio <eduavenancio@gmail.com>
2025-11-13 00:27:36 -0300

    ex.23 Corrigido
    Adicionei 2 while True dentro de outro while True para ser adiocionado apenas as 'str' previstas, para não causar bugs.
    Utilizei o 'for c in cadastros' para que o 'c' se tornasse cada dicionário de dentro da lista, e ficasse mais simplificado e mais limpo o código.

--- python_ex3/desafio23.py
+++ python_ex3/desafio23.py
@@ -1,34 +1,43 @@
 cadastros = []
 pessoa = {}
-grupotot = media_idadetot = cont = contm = 0
+media_idadetot = cont = contm = 0
 while True:
+    pessoa.clear()
     pessoa['nome'] = str(input('Nome:'))
-    pessoa['sexo'] = str(input('Sexo (M/F): ')).upper().strip()[0]
+    while True:
+        pessoa['sexo'] = str(input('Sexo (M/F): ')).upper().strip()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('Erro! Por Favor Digite Apenas (M) Ou (F)!')
     pessoa['idade'] = int(input('Idade: '))
-    grupotot += 1
+    media_idadetot += pessoa['idade']
     cadastros.append(pessoa.copy())
-    continuar = str(input('Quer continuar (S/N)?')).upper().strip()[0]
+    while True:
+        continuar = str(input('Quer continuar (S/N)?')).upper().strip()[0]
+        if continuar in 'SN':
+            break
+        print('Erro! Por Favor Digite Apenas (S) Ou (N)!')
     if continuar == 'N':
         break
 print('=-'*20)
-print(f'- O grupo tem {grupotot} pessoas.')
-for c in range(0, grupotot):
-    media_idadetot += cadastros[c]['idade']
-media_idadetot = media_idadetot / grupotot
+print(f'- O grupo tem {len(cadastros)} pessoas.')
+media_idadetot = media_idadetot / len(cadastros)
 print(f'- A média de idade é {media_idadetot:.0f}.')
 print(f'- As mulheres cadastradas foram:', end=' ')
-for c in range(0, grupotot):
-    for p in cadastros[c]['sexo']:
+for c in cadastros:
+    for p in c['sexo']:
         if p == 'F':
-            print(f'{cadastros[c]["nome"]}', end=' ')
+            print(f'{c["nome"]}', end=' ')
             contm = 1
 if contm == 0:
     print('Nenhuma mulher cadastrada!', end=' ')
 print('\n- Lista de pessoas que estão acima da média:\n')
-for c in range(0, grupotot):
-    if cadastros[c]['idade'] > media_idadetot:
-        print(cadastros[c])
-        print()
+for c in cadastros:
+    if c['idade'] >= media_idadetot:
         cont = 1
+        for v, k in c.items():
+            print(f'{v} == {k}', end='; ')
+print()
 if cont == 0:
     print('Não tem pessoas acima da média!')
+print(f'{"<< Encerrado >>":^35}')
