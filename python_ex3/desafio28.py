a932e0c7bd670be0d27def6c5ffdeba1fa9ad532
Eduardo Venancio <eduavenancio@gmail.com>
2025-11-19 16:22:46 -0300

    ex.28 Função que descobre o maior valor.
    A função pega os valores de cada chamada, de fora da função e retonar informando quantos valores tinham em cada chamada, e qual é o maior valor de cada chamada.

---
+++ python_ex3/desafio28.py
@@ -0,0 +1,21 @@
+from time import sleep
+def maior(*valor):
+    print('__'*25)
+    print('Analisando os valores passados...')
+    num = 0
+    for c, n in enumerate(valor):
+        if c == 0:
+            num = n
+        if num <= n:
+            num = n
+        print(n, end=' ')
+        sleep(0.4)
+    print(f'Foram informados {len(valor)} valores ao todo.')
+    print(f'O maior valor informado foi {num}.')
+
+
+maior(2, 9, 4, 5, 7, 1)
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
+maior()
