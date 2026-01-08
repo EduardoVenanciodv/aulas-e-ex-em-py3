3365bb8441dddc669f247b6c27e54e6b8ab13877
Eduardo Venancio <eduavenancio@gmail.com>
2026-01-08 14:21:31 -0300

    ex30 Função para votação.
    ajuste no elif, retirado um 'or' trocado por '16 <= idade == 17'.
    'from datetime import date' foi introduzido diretamente dentro da função voto.

--- python_ex3/desafio30.py
+++ python_ex3/desafio30.py
@@ -1,15 +1,14 @@
-from datetime import date
-
 def voto(a):
-    idade = data.year - a
+    from datetime import date
+    data = date.today().year
+    idade = data - a
     if idade < 16:
         print(f'Com {idade} anos: Não vota!')
-    elif idade == 16 or idade == 17 or idade > 69:
+    elif 16 <= idade == 17 or idade > 69:
         print(f'Com {idade} anos: Voto opcional!')
     else:
         print(f'Com {idade} anos: Voto obrigatório!!')
 
 
 print('__'*13)
-data = date.today()
 voto(int(input('Que ano você nasceu?')))
