6082758a04f6042117b16cee9c6b6db436c6c6d5
Eduardo Venancio <eduavenancio@gmail.com>
2026-01-21 18:18:36 -0300

    ex32 ficha do jogador
    Corrigido.
    Agora se o usuário escreve utilizando str, o programa coloca o valor de gol pra 0.

--- python_ex3/desafio32.py
+++ python_ex3/desafio32.py
@@ -1,16 +1,21 @@
 
-def ficha(nome, gol):
+def ficha(nome = '<desconhecido>', gol = 0):
     """
     :param nome: Nome do jogador. Se o usuário não adicionar um nome, automaticamente irar ficar como deconhecido.
     :param gol: Gols do jogador. Se o usuário não adicionar valor, automaticamente irar ficar com 0 gols.
     :return: sem retorno
     """
-    if nome == '':
-        nome = '<desconhecido>'
-    if gol == '':
-        gol = 0
     print(f'O jogador {nome}, tem {gol} gol(s) no campeonato.')
 
 
 print('__'*10)
-ficha(nome=str(input('Nome do jogador:')), gol=str(input('Número de gols:')))
+n = str(input('Nome do jogador:'))
+g = str(input('Número de gols:'))
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if n.strip() == '':
+    ficha(gol=g)
+else:
+    ficha(n, g)
