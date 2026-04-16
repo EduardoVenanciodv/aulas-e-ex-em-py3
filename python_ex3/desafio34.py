1e22db531d11dfa4bfe1bb8f92b7572643d3908e
Eduardo Venancio <eduavenancio@gmail.com>
2026-04-16 20:54:08 -0300

    update ex 34
    Código mais limpo

--- python_ex3/desafio34.py
+++ python_ex3/desafio34.py
@@ -1,36 +1,30 @@
 
 def notas(* n, sit=False):
+    """"
+    -->Função para analisar todas as notas e situação dos alunos
+    :param n: recebe uma ou mais notas dos alunos
+    :param sit: valor opcional, indicando se deve mostra a situção do aluno
+    :return : dicionário com várias informções sobre a situação da turma
+    """
     planilha = dict()
     planilha['total'] = len(n)
-    cont = 0
-    total = 0
-    for c in n:
-        if cont == 0:
-            menor = c
-            maior = c
-            cont += 1
-        else:
-            if c > maior:
-                maior = c
-            if c < menor:
-                menor = c
-        total += c
-    mediaT = total / len(n)
-    planilha['Maior nota'] = maior
-    planilha['Menor nota'] = menor
-    planilha['Media'] = mediaT
-    if mediaT == 10 and True is sit:
-        planilha['Situação'] = 'Perfeita'
-    if 8.5 <= mediaT < 10 and True is sit:
-        planilha['Situação'] = 'Muito Boa'
-    if 7 <= mediaT < 8.5 and True is sit:
-        planilha['Situação'] = 'Boa'
-    if 5 <= mediaT < 7 and True is sit:
-        planilha['Situação'] = 'Mediana'
-    if 0 <= mediaT < 5 and True is sit:
-        planilha['Situação'] = 'Ruim'
+    planilha['Maior nota'] = max(n)
+    planilha['Menor nota'] = min(n)
+    planilha['Media'] = sum(n) / len(n)
+    if sit:
+        if planilha['Media'] == 10:
+            planilha['Situação'] = 'Perfeita'
+        if 8.5 <= planilha['Media'] < 10:
+            planilha['Situação'] = 'Muito Boa'
+        if 7 <= planilha['Media'] < 8.5:
+            planilha['Situação'] = 'Boa'
+        if 5 <= planilha['Media'] < 7:
+            planilha['Situação'] = 'Mediana'
+        if 0 <= planilha['Media'] < 5:
+            planilha['Situação'] = 'Ruim'
     return planilha
 
 
-resp = notas(3, 2, 4, 1, sit=True)
+resp = notas(4, 2, 8, 6, sit=True)
 print(resp)
+help(resp)
