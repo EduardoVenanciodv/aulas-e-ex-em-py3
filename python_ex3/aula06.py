24e7bf1c8b50977a413a774018137068379c0eae
Eduardo Venancio <eduavenancio@gmail.com>
2026-05-02 17:53:18 -0300

    Aula06 Modulos

---
+++ python_ex3/aula06.py
@@ -0,0 +1,7 @@
+import aula06uteis
+ 
+num = int(input('Digite um número: '))
+fat = aula06uteis.fatorial(num)
+print(f'O fatorial de {num} é {fat}')
+print(f'O dobro de {num} é {aula06uteis.dobro(num)}')
+print(f'O triplo de {num} é {aula06uteis.triplo(num)}')
