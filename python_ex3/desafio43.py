241fd0fca75c5f03d96cf315e6b5ec651b414f50
Eduardo Venancio <eduavenancio@gmail.com>
2026-05-20 16:53:06 -0300

    ex 43
    testando a abertura de um sit

--- python_ex3/desafio43.py
+++ python_ex3/desafio43.py
@@ -1,8 +1,9 @@
+from urllib import error
 import urllib.request
 
 try:
-    site = urllib.request.urlopen("https://www.detran.rj.gov.br")
-except:
+    site = urllib.request.urlopen('https://www.youtube.com')
+except urllib.error.URLError:
     print('Não consegui me conectar')
 else:
     print('Conectado com sucesso')
